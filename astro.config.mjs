f3a9799577dffb7f9ec0405c179c833e7682bc1f
Padmanand Warrier <padmanand@outlook.com>
2025-12-26 07:31:38 +0530

    Standardized slugs for consistent formatting of all articles. Added Zambia lab service content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-   export default defineConfig({
-     site: 'https://padmanandwarrier.com',
-     base: '/',
-     output: 'static',
-   });
+export default defineConfig({
+  site: 'https://padmanandwarrier.com',
+  base: '/',
+  output: 'static',
+  integrations: [mdx()],
+});
